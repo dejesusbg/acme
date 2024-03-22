4350577dd9294b85ff91372c092f0cad2762e811
ricardo barrios <110137626+dejesusbg5@users.noreply.github.com>
2024-03-21 22:10:12 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 colorScheme("#2b506d", "#d83a55");
 toggleDarkMode();
 
+// sufrage list
 function voteDialog(e) {
   e.onclick = () => {
+    let info = document.getElementById("acme-student-info");
+    info.innerHTML =
+      e.children[0].innerHTML + "<br />" + e.children[1].innerHTML + " (9°)";
+
     openDialog("info-dialog");
 
     let btn = document.getElementById("allow-vote");
     let dialog = document.getElementById("info-dialog");
+    let nVotes = document.getElementById("acme-votes-amt");
 
     let studentVoted = () => {
       e.classList.add("acme-table--voted");
@@ -14,6 +20,9 @@ function voteDialog(e) {
 
       btn.innerHTML = '<span class="md3-btn__text"> Permitir </span>';
       btn.disabled = false;
+
+      nVotes.innerHTML =
+        document.getElementsByClassName("acme-table--voted").length;
 
       customCloseDialog(dialog);
       closeDialog();
@@ -33,3 +42,58 @@ function voteDialog(e) {
 document
   .querySelectorAll("tr:not(.acme-table--voted):nth-child(n + 2)")
   .forEach((e) => voteDialog(e));
+
+// read more on news
+let news = document.getElementsByClassName("acme-news");
+Array.from(news).forEach((n) => {
+  n.onclick = () => {
+    Array.from(news).forEach((nr) => {
+      n != nr && nr.classList.remove("acme-news--read");
+    });
+
+    n.classList.toggle("acme-news--read");
+  };
+});
+
+// log in as estudiante or jurado
+function login(form) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  let userType = 0;
+  if (urlParams.size) {
+    userType = urlParams.get("userType");
+  }
+
+  let userEmail = document.getElementById("user");
+  let nextPage = userType == "1" ? "./sufrage.html" : "./vote.html";
+
+  // form.action = nextPage + "?u=" + userEmail.value;
+  form.action = nextPage;
+  return true;
+}
+
+// filter table
+function filterTable(input) {
+  let filter, table, tr, td, i, txtValue;
+  filter = input.value.toUpperCase();
+  table = document.getElementById("acme-students");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    for (j = 0; j < 2; j++) {
+      td = tr[i].getElementsByTagName("td")[j];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "table-row";
+          break;
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+  return true;
+}
